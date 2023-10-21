40697da5030aed4729579c347af8d3f125edae09
Yingying Feng <yfeng59@my.bcit.ca>
2023-10-21 14:17:11 -0700

    Removed react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
 import React from "react";
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './components/Home';
 
 function App() {
   return (
-    <Router basename='/'>
-      <Routes>
-        <Route path='/' exact element={<Home />}/>
-      </Routes>
-    </Router>
+    <Home/>
   );
 }
 
